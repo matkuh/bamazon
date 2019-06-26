e357a9e797c3ec6ee31a0571198491324995e842
matkuh <matkuh@gmail.com>
2019-06-26 10:07:07 -0700

    fix display item function, add purchase item function, use callbacks to get functions to run in order

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -13,59 +13,87 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
+var table = new Table({
+    head: ['ID', 'Name', 'Department', 'Price', 'Stock Quantity']
+    , colWidths: [10, 20, 20, 20, 20]
+})
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // displayItems();
+    displayItems(purchaseitems);
     // purchaseitems();
 });
 
-var table = new Table({
-    head: ['ID', 'Name', 'Department', 'Price', 'Stock Quantity']
-  , colWidths: [10, 20, 20, 20, 20]
-})
-
-displayItems();
-
-var userquantity;
+function displayItems(cb) {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        table = new Table({
+            head: ['ID', 'Name', 'Department', 'Price', 'Stock Quantity']
+            , colWidths: [10, 20, 20, 20, 20]
+        })
+        for (var i = 0; i < res.length; i++) {
+            table.push([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        console.log(table.toString());
+        cb()
+    })
+}
 
 function purchaseitems() {
     inquirer.prompt([
         {
             message: "Enter the ID of the item you want to buy?",
-            name: "what_id"
+            name: "what_id",
+            type: "input"
         },
         {
             message: "How many units do you want to buy?",
-            name: "number_units"
+            name: "number_units",
+            type: "input"
         }
     ])
         .then(function (response) {
-            updateItems();
-             }
-        )}
-
-
-function displayItems(){
-    connection.connect("SELECT * FROM products", function(err, res){
-        if (err) throw err;
-        console.log("-------------------------------------------")
-        console.log(res)
-        connection.end();
-    })
-}
-
-function updateItems() {
-    connection.query("SELECT * FROM products WHERE item_id =?", [response.what_id], function (err, res) {
-        if (err) throw err;
-        if (parseFloat(response.number_units) > res[0].stock_quantity) {
-            connection.query("UPDATE product SET stock_quantity =?", [res.stock_quantity - response.number_units], function (err, res) {
-                console.log("Your total cost is: " + (parseFloat(response.number_units) * (res.price)))
+            console.log(response.what_id)
+            console.log(response.number_units)
+            connection.query("SELECT * FROM products WHERE id=?", [response.what_id], function (err, resp) {
                 if (err) throw err;
+                // console.log(resp);
+                var usernumunits = parseInt(response.number_units)
+                if (usernumunits <= resp[0].stock_quantity) {
+                    connection.query("UPDATE products SET stock_quantity =? WHERE id=? ", [(parseInt(resp[0].stock_quantity) - usernumunits), response.what_id], function (err, res) {
+                        console.log("Your total cost is: " + (usernumunits * resp[0].price))
+                        // if (err) throw err;
+                    });
+                }
+                else {
+                    console.log("Not enough in Stock!");
+                }
+                displayItems(buyanother);
+                // buyanother();
             });
         }
-        else {
-            console.log("Not enough in Stock!");
+        )
+}
+
+function buyanother() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Would you like to buy another item?",
+            choices: ["Yes", "No"],
+            name: "selection"
         }
-    });
+    ])
+        .then(function (response) {
+            switch (response.selection) {
+                case "Yes":
+                    displayItems(purchaseitems);
+                    break;
+
+                case "No":
+                    connection.end();
+                    break;
+            }
+        });
 }
